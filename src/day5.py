4817f87864d5bb5d7fc5b7dec83ba997c8bb1c80
Benjamin Strauch <6814712+bestrauc@users.noreply.github.com>
2023-12-16 14:20:08 +0100

    Finally, I'm free.

--- src/day5.py
+++ src/day5.py
@@ -9,7 +9,10 @@ import utils
 
 @dataclass
 class RangeMap:
-    """Map a range as described in the task."""
+    """Map a range as described in the task.
+
+    Maps [source, source_end) -> [target, target_end)
+    """
 
     source: int
     target: int
@@ -21,7 +24,7 @@ class RangeMap:
             return key
 
         # e.g. with (50, 14), 51 (51-50 = 1) will map to 14+1 = 15
-        return self.target + (key-self.source)
+        return self.target + (key - self.source)
 
     def is_in_range(self, key) -> bool:
         return self.source <= key < self.source_end
@@ -42,9 +45,14 @@ class RangeMap:
     def __repr__(self):
         return f"{self.source} {self.target} {self.size}"
 
+
 @dataclass
 class RangeMapUnion:
-    """Consider all ranges for mapping, falling back on the identity if none works."""
+    """Consider all ranges for mapping, falling back on the identity if none works.
+
+    Not a very useful class in retrospect - didn't use it for part 2.
+    """
+
     range_maps: list = field(default_factory=list)
 
     def __getitem__(self, key: int) -> int:
@@ -53,6 +61,7 @@ class RangeMapUnion:
                 return range_map[key]
 
         return key
+
 
 def parse_puzzle_input(puzzle_input: list[str]) -> tuple[list, dict]:
     almanach_maps = {}
@@ -75,25 +84,83 @@ def parse_puzzle_input(puzzle_input: list[str]) -> tuple[list, dict]:
 
     return seeds, almanach_maps
 
+
 def seed_to_location(seed: int, almanach_maps: dict) -> int:
     # Rely on the maps being already ordered in the input
     mapped_value = seed
     for map_type, range_map in almanach_maps.items():
         mapped_value = range_map[mapped_value]
-    
+
     return mapped_value
 
+## Part 
 puzzle_input = utils.read_puzzle_input(sys.argv[1])
 seeds, almanach_maps = parse_puzzle_input(puzzle_input)
 
+# Part 1 ---------------------
 print(min(seed_to_location(seed, almanach_maps) for seed in seeds))
-sys.exit()
 
-# This doesn't work for part 2 of course.
-seed_ranges = list(zip(seeds[::2], seeds[1::2]))
-def is_in_seeds(val: int) -> bool:
-    for seed_start, seed_length in seed_ranges:
-        if seed_start <= val < seed_start+seed_length:
-            return True
-    
-    return False
+# Part 2 ---------------------
+def map_range(input_range: tuple[int, int], range_map: RangeMap) -> tuple[list, list]:
+    """Map interval [a,b) with the map whose domain is [c,d).
+
+    Returns a list of identity-mapped ranges and a list with the mapped range.
+    """
+    start, end = input_range
+
+    #  --
+    # ----
+    if range_map.source <= start < end <= range_map.source_end:
+        ret = ([], [(range_map[start], range_map[end - 1] + 1)])
+    # ---              ---
+    #     ---  or  ---
+    elif (end <= range_map.source) or (range_map.source_end <= start):
+        ret = ([(start, end)], [])
+    # ----
+    #  --
+    elif start <= range_map.source < range_map.source_end <= end:
+        initial = [(start, range_map.source)] if start < range_map.source else []
+        tail = [(range_map.source_end, end)] if range_map.source_end < end else []
+        ret = ([*initial, *tail], [(range_map.target, range_map.target_end)])
+    # ----
+    #   ----
+    elif start < range_map.source < end < range_map.source_end:
+        ret = (
+            [(start, range_map.source)],
+            [(range_map[range_map.source], range_map[end - 1] + 1)],
+        )
+    #   ----
+    # ----
+    elif range_map.source < start < range_map.source_end < end:
+        ret = [(range_map.source_end, end)], [
+            (range_map[start], range_map[range_map.source_end - 1] + 1)
+        ]
+    else:
+        assert False
+
+    assert all(a < b for a, b in ret[0] + ret[1]), ret
+    return ret
+
+
+
+seed_ranges = sorted([(a, a + b) for a, b in zip(seeds[::2], seeds[1::2])])
+
+for map_name, maps in almanach_maps.items():
+    next_maps_seeds = []
+
+    for m in maps.range_maps:
+        remaining_seeds = []
+        for seed_range in seed_ranges:
+            identity_ranges, mapped_ranges = map_range(seed_range, m)
+
+            # The unmapped regions can be tried in the next maps.
+            remaining_seeds.extend(identity_ranges)
+
+            # The mapped regions go in the next round.
+            next_maps_seeds.extend(mapped_ranges)
+
+        seed_ranges = remaining_seeds
+
+    seed_ranges = sorted(next_maps_seeds + remaining_seeds)
+
+print(seed_ranges[0][0])
